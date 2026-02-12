78721699ffb167681c2822a61f7fb8d563f59371
ethCS <syrup199@gmail.com>
2026-02-11 21:32:47 -0700

    Break syntax intentionally

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-def conversion(value):
+def conversion(value)
     return value * 9 / 5 + 32
 
 
-def is_strong(values):
+def is_strong(values)
     return sum(values) / len(values) > 50
 
 
-def average(values):
+def average(values)
     return sum(values) / len(values)

da927a19520f6f3f2edaaed3609860f12cc7de0d
ethCS <syrup199@gmail.com>
2026-02-11 21:40:14 -0700

    Trigger CI for branch protection

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@ def conversion(value)
     return value * 9 / 5 + 32
 
 
-def is_strong(values)
+def is_strong(values):
     return sum(values) / len(values) > 50
 
 
-def average(values)
+def average(values):
     return sum(values) / len(values)
